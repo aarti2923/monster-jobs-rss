4afc8aff29dfe7f876b5b3f452eeb9720b5c0979
pawanmethre <pawanmethre@gmail.com>
2020-03-06 18:31:34 +0530

    adding more categories to rss_cat

--- rss_app/models.py
+++ rss_app/models.py
@@ -5,9 +5,9 @@ from first.utils.postgres_connection import Postgresql
 
 class RssCategory():
 	def rss_cat_data(self):
-		#311, 1274, 1282,1270, 20
+		#311, 1274, 1282,1270, 20, 15,1283,1265,1279,1256,1261,1284,1266,1280,1262,1271,1276,1267,1258,1272,1281,1263,1277,1268,1250
 		#1251,1274,22,907,311,1282,13,18,1270,5,20,2,786,1000,6,10,17,785,1071,908,19,24,15,1283,1265,1279,1256,1247,1261,1252,1284,1257,1266,1280,1248,1262,1271,1253,1276,1267,1258,1272,1281,1263,1277,1268,1250
-		data=Postgresql().get_all("""SELECT * from public.rss_feed_cat where cat_id in (1251,22,907,13,18,5,2);""")
+		data=Postgresql().get_all("""SELECT * from public.rss_feed_cat where cat_id in (1251,22,907,13,18,5,2, 786,1000,6,10,17,785,1071,908,19,24,1247,1252,1257,1248,1253);""")
 		# print(data)
 		return data
 
@@ -30,13 +30,13 @@ class  CreateTable():
 			from ast import literal_eval
 			import json
 			headers = {'Content-Type':"application/json"}
-			info = requests.get('http://192.168.0.145:8000/api/rss_data/?cat_id=2', headers=headers).json()
+			info = requests.get('http://192.168.0.145:8000/api/rss_data/?cat_id=1250', headers=headers).json()
 			# info = info.split('response')[1]
 			for each in info['response']:
 				print('pp',each['company'],each['experience'],each['salary'],each['location'],each['qualification'])
 				try:
 					response = Postgresql().execute_query("""INSERT INTO public.rss_data(company,experience,salary, location,qualification,cat_id) 
-					values('{}','{}','{}','{}','{}',2)""".format(each['company'],each['experience'],each['salary'],each['location'],each['qualification']))
+					values('{}','{}','{}','{}','{}',1250)""".format(each['company'],each['experience'],each['salary'],each['location'],each['qualification']))
 				except:
 					raise
 			# Postgresql().execute_query("""CREATE TABLE IF NOT EXISTS public.rss_data (id bigserial NOT NULL,company varchar(200),experience varchar(200),salary varchar(200),location varchar(200),qualification varchar(200),cat_id integer,loan boolean default false,PRIMARY KEY (id))""")
